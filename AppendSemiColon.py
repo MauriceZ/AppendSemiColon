5c77a20d39a079fed9f1841bc35258d609f3262d
Maurice Zhang <Maurice@Maurices-MacBook-Air.local>
2015-06-24 13:07:07 -0400

    Add feature to only place semicolon if there isn't already one

--- AppendSemiColon.py
+++ AppendSemiColon.py
@@ -4,4 +4,5 @@ class AppendSemiColonCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		for region in self.view.sel():
 			line_end = self.view.line(region).end()
-			self.view.insert(edit, line_end, ';')
+			if self.view.substr(line_end - 1) != ';':
+				self.view.insert(edit, line_end, ';')
